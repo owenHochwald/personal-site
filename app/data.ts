1538b2397edb3256c9d64aa37530e0ef45d8b151
owenHochwald <owenhochwald@gmail.com>
2025-02-16 12:59:57 -0500

    update videos

--- app/data.ts
+++ app/data.ts
@@ -44,7 +44,8 @@ export const PROJECTS: Project[] = [
       'Custom trained transformer to generate original Shakespearean-style text.',
     link: 'https://github.com/owenHochwald/shakespeare-gpt/',
     video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
+    //   'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
+    '/gpt_demo.mp4',
     id: 'project1',
   },
   {
@@ -52,7 +53,8 @@ export const PROJECTS: Project[] = [
     description: 'Open-source, company-wide time-tracking end-to-end web app.',
     link: 'https://www.aftrbrnr.com/',
     video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
+    //   'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
+    '/aftrbrnr_demo.mp4',
     id: 'project2',
   },
   {
@@ -60,7 +62,8 @@ export const PROJECTS: Project[] = [
     description: 'Chrome extension to promote better posture habits with personalized reminders and friendly cat animations.',
     link: 'https://chromewebstore.google.com/detail/Pawsture%20Pal/ghdnbollliflcjoeglpnlecdhcbcbopf',
     video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
+    //   'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
+    '/extension_demo.mp4',
     id: 'project2',
   },
   {
@@ -69,7 +72,7 @@ export const PROJECTS: Project[] = [
       'Platform to connect university students via real-time video chat.',
     link: 'https://devpost.com/software/student-connect-bvz1it',
     video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
+      'webrtc_demo.mp4',
     id: 'project1',
   },
 ]
