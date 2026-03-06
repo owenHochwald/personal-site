999684a005452ec3ca1dae934ea9f81c03966919
owenhochwald <owenhochwald@gmail.com>
2026-03-05 22:51:14 -0800

    add deps for mdx full support

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 import createMDX from '@next/mdx';
+import remarkGfm from 'remark-gfm';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -11,6 +12,9 @@ const nextConfig = {
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+  },
 });
 
 export default withMDX(nextConfig);
