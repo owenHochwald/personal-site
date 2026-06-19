08f316f28618029a233c7edcb2ccf640197fe607
Owen Hochwald <62612083+owenHochwald@users.noreply.github.com>
2026-06-19 11:59:08 -0400

    change runtime and fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata, Viewport } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { Header } from './header'
 import { Footer } from './footer'
@@ -31,15 +30,6 @@ export const metadata: Metadata = {
     },
 }
 
-const geist = Geist({
-    variable: '--font-geist',
-    subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-    variable: '--font-geist-mono',
-    subsets: ['latin'],
-})
 
 export default function RootLayout({
     children,
@@ -47,10 +37,8 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     return (
-        <html lang="en" suppressHydrationWarning>-
-            <body
-                className={`${geist.variable} ${geistMono.variable} bg-white tracking-tight antialiased dark:bg-zinc-950`}
-            >
+        <html lang="en" suppressHydrationWarning>
+            <body className="bg-white tracking-tight antialiased dark:bg-zinc-950 font-sans">
 
                 <ThemeProvider
                     enableSystem={true}
@@ -59,7 +47,7 @@ export default function RootLayout({
                     defaultTheme="system"
                 >
 
-                    <div className="flex min-h-screen w-full flex-col font-[family-name:var(--font-inter-tight)]">
+                    <div className="flex min-h-screen w-full flex-col">
                         <div className="fixed inset-0 -z-10">
                             <Particles className="absolute h-full w-full" />
                         </div>
